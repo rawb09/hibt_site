8845cc8313cacc17a0560ed33a1ead2bfaeff910
Dream Corps <dreamcorps@Spare-MacBook-Air-4.local>
2018-01-24 15:34:27 -0800

    fixed rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :lastseenable, :onliner
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :reviews
-
- def online
-    @online = User.where('last_seen > ?', 2.minutes.ago)
-  end
 
 end
 
